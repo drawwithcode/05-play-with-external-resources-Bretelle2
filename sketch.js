4aecc64f2cac0e1eb26b3d83d99d8e046b068b65
Francesco Longoni <fra-long96@hotmai.it>
2018-10-23 19:48:38 +0200

    finito?

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
-function preload(){
-  // put preload code here
+var api = "https://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+var query = "&q=harambe";
+var song;
+
+function preload() {
+song = loadSound("harambe_song.mp3")
 }
 
 function setup() {
-  // put setup code here
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+  song.play()
+  createCanvas(200, 200)
 }
 
-function draw() {
-  // put drawing code here
+function draw () {
+  textFont('Gloria Hallelujah')
+  textSize(15)
+  text('is this a meme?', 10, 10);
+}
+
+function gotData(giphy) {
+  for (var i = 0; i <= 10; i++) {
+    createImg(giphy.data[0].images.original.url);
+    createImg(giphy.data[10].images.original.url);
+  }
+
 }
